541ac2280b10e89d91e5616d4f8684f1d21c755d
contactindrvx-cmyk <contact.indrvx@gmail.com>
2026-04-17 04:56:34 +0500

    Update MyWebChromeClient.java

--- java_src/org/alien/MyWebChromeClient.java
+++ java_src/org/alien/MyWebChromeClient.java
@@ -1,12 +1,10 @@
 package org.alien;
-
 import android.webkit.WebChromeClient;
 import android.webkit.PermissionRequest;
 
 public class MyWebChromeClient extends WebChromeClient {
     @Override
     public void onPermissionRequest(final PermissionRequest request) {
-        // یہ جادوئی لائن WebView کی تمام پرمیشنز کو خاموشی سے الاؤ کر دے گی
         request.grant(request.getResources());
     }
 }
